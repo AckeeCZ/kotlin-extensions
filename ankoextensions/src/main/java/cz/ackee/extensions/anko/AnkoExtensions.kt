12ca75a4448a8525ac3c8e3398bdf70900358443
David Bilik <david.bilik@ackee.cz>
2017-05-03 15:10:37 +0200

    Add kotlin plugin to ankoextensions

--- ankoextensions/src/main/java/cz/ackee/extensions/anko/AnkoExtensions.kt
+++ ankoextensions/src/main/java/cz/ackee/extensions/anko/AnkoExtensions.kt
@@ -8,6 +8,7 @@ import android.support.v7.widget.AppCompatRadioButton
 import android.support.v7.widget.AppCompatSeekBar
 import android.view.ViewGroup
 import android.view.ViewManager
+import android.widget.LinearLayout
 import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.custom.ankoView
 
@@ -25,43 +26,6 @@ import org.jetbrains.anko.custom.ankoView
 inline fun ViewManager.textInputEditText() = textInputEditText {}
 
 inline fun ViewManager.textInputEditText(theme: Int = 0, init: TextInputEditText.() -> Unit) = ankoView(::TextInputEditText, theme, init)
-
-/**
- * Anko extensions for [AppCompatEditText]
- */
-inline fun ViewManager.appcompatEdittext() = appcompatEdittext {}
-
-inline fun ViewManager.appcompatEdittext(theme: Int = 0, init: AppCompatEditText.() -> Unit) = ankoView(::AppCompatEditText, theme, init)
-
-/**
- * Anko extensions for [AppCompatSeekBar]
- */
-inline fun ViewManager.appcompatSeekbar() = appcompatSeekbar {}
-
-inline fun ViewManager.appcompatSeekbar(theme: Int = 0, init: AppCompatSeekBar.() -> Unit) = ankoView(::AppCompatSeekBar, theme, init)
-
-
-/**
- * Anko extensions for [AppCompatRadioButton]
- */
-inline fun ViewManager.appcompatRadiobutton() = appcompatRadiobutton {}
-
-inline fun ViewManager.appcompatRadiobutton(theme: Int = 0, init: AppCompatRadioButton.() -> Unit) = ankoView(::AppCompatRadioButton, theme, init)
-
-/**
- * Anko extensions for [AppCompatCheckBox]
- */
-inline fun ViewManager.appcompatCheckbox() = appcompatCheckbox {}
-
-inline fun ViewManager.appcompatCheckbox(theme: Int = 0, init: AppCompatCheckBox.() -> Unit) = ankoView(::AppCompatCheckBox, theme, init)
-
-/**
- * Anko extensions for [BottomNavigationView]
- */
-inline fun ViewManager.bottomNavigation() = bottomNavigation {}
-
-inline fun ViewManager.bottomNavigation(theme: Int = 0, init: BottomNavigationView.() -> Unit) = ankoView(::BottomNavigationView, theme, init)
-
 
 /**
  * Get Anko context from view group
