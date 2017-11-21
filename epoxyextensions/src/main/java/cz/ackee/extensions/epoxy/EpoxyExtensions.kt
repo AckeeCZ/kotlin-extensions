7db43285eb846925aaaf0304c756cb962e9146ee
David Bilik <david.bilik@ackee.cz>
2017-11-21 16:48:17 +0100

    Add epoxy

---
+++ epoxyextensions/src/main/java/cz/ackee/extensions/epoxy/EpoxyExtensions.kt
@@ -0,0 +1,23 @@
+package cz.ackee.extensions.epoxy
+
+import com.airbnb.epoxy.EpoxyController
+import kotlin.properties.ObservableProperty
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ * Extensions for EpoxyController
+ *
+ * @author David Bilik [david.bilik@ackee.cz]
+ * @since 21/11/2017
+ **/
+
+/**
+ * Returns a property delegate for a read/write property that calls [EpoxyController#requestModelBuild] method when property is changed
+ * @param initialValue the initial value of the property.
+ */
+fun <T> EpoxyController.adapterProperty(initialValue: T): ReadWriteProperty<Any?, T> = object : ObservableProperty<T>(initialValue) {
+    override fun afterChange(property: KProperty<*>, oldValue: T, newValue: T) {
+        requestModelBuild()
+    }
+}
