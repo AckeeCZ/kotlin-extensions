b6c951b81977811523a8dfd39626e389c898fb88
Georgiy Shur <georgiy.shur@ackee.cz>
2018-01-29 12:27:24 +0100

    Color getters on fragment

--- androidextensions/src/main/java/cz/ackee/extensions/android/FragmentExtensions.kt
+++ androidextensions/src/main/java/cz/ackee/extensions/android/FragmentExtensions.kt
@@ -1,6 +1,8 @@
 package cz.ackee.extensions.android
 
 import android.os.Bundle
+import android.support.annotation.ColorRes
+import android.support.annotation.IntRange
 import android.support.v4.app.Fragment
 
 /**
@@ -26,3 +28,13 @@ fun Fragment.withTargetFragment(fragment: Fragment, reqCode: Int): Fragment {
     setTargetFragment(fragment, reqCode)
     return this
 }
+
+/**
+ * Get color from resource with fragment context.
+ */
+fun Fragment.color(@ColorRes res: Int) = context!!.color(res)
+
+/**
+ * Get color from resource with fragment context and apply alpha to it.
+ */
+fun Fragment.colorWithAlpha(@ColorRes res: Int, @IntRange(from = 0, to = 100) alphaPercent: Int) = context!!.colorWithAlpha(res, alphaPercent)
