d965ef51d5371a1bab1fc6b4e6a9af6e340e8db0
David Bilik <david.bilik@ackee.cz>
2018-06-05 16:42:56 +0200

    Fix typo in javadoc

--- androidextensions/src/main/java/cz/ackee/extensions/android/Common.kt
+++ androidextensions/src/main/java/cz/ackee/extensions/android/Common.kt
@@ -16,7 +16,7 @@ fun Int.toBoolean() = this > 0
 fun Boolean.toInt() = if (this) 1 else 0
 
 /**
- * Assume that this Int is an color and apply [opacity] to it.
+ * Assume that this Int is a color and apply [opacity] to it.
  */
 @ColorInt
 fun Int.withOpacity(@IntRange(from = 0, to = 100) opacity: Int): Int {
