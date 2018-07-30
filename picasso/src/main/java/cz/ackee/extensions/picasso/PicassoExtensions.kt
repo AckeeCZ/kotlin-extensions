7c2f319c54fef4d2b45d0f57105f91e6ab830bc0
Petr Kubáč <petr.kubac@ackee.cz>
2018-07-30 15:19:02 +0200

    add optional no fade to load url

--- picasso/src/main/java/cz/ackee/extensions/picasso/PicassoExtensions.kt
+++ picasso/src/main/java/cz/ackee/extensions/picasso/PicassoExtensions.kt
@@ -7,8 +7,12 @@ import com.squareup.picasso.Picasso
 // Extensions for Picasso
 
 /**
- * Load url to ImageView and use optional callback
+ * Load url to ImageView and use optional callback or noFade to disable fade animation
  */
-fun ImageView.loadUrl(url: String, callback: Callback? = null) {
-    Picasso.with(context).load(url).into(this, callback)
+fun ImageView.loadUrl(url: String, noFade: Boolean = false, callback: Callback? = null) {
+    if (noFade) {
+        Picasso.with(context).load(url).noFade().into(this, callback)
+    } else {
+        Picasso.with(context).load(url).into(this, callback)
+    }
 }
